cb0a89c2c520b78ada656a87ef7209f8ffa032f8
Julieta Ruiz Simancas <julietaruizsimancas@gmail.com>
2022-09-13 10:49:57 +0200

    se incorpora desafio

--- Desafio-complementario-1/index.js
+++
@@ -1,5 +0,0 @@
-for (let i = 1; i<=8; i ++){
-        let nombre = prompt ("Ingrese su nombre");
-        let mensaje= `Su reserva es la #${i} A nombre de:${nombre}`;
-        alert (mensaje);
-    };
---
+++ Desafio-simulador-version2/index.js
@@ -0,0 +1,38 @@
+let product = 0;
+let amount= 0;
+let price=0;
+let total=0;
+
+function sale () {
+    while ( product ===0 || product > 1 || !product){
+        product = parseInt (prompt( "Ingrese el codigo del producto elegido"));
+    }
+    switch (product){
+        case 1:
+            product = "Masilla Ceramica TNX24";
+            price = 1250;
+            break;
+        case 2:
+            product = "Masilla Ceramica TNX35";
+            price = 1670;
+            break;
+        case 3:
+            product = "Masilla Alumina ALUMIIK";
+            price = 2500;
+            break;
+        case 4:
+            product = "Masilla Ceramica TNX62";
+            price = 1920;
+            break;
+    }   
+    while (amonunt ===0 || !amount)  {
+        amount = parseInt ( prompt ( "Indique la cantidad de " + product+ "desea llevar."));
+    }      
+}
+function finalAcount (){
+    total = amount * price;
+    alert ( "Ha comprado" + amount+ +product+ "Muchas gracias por su compra, a la brevedad recibira los productos.");
+}
+
+sale ()
+finalAcount ()
